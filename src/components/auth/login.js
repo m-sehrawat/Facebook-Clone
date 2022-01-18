72b8976dcc2f4265a12ab9bc58fa7dbbea835df7
swarnikaraj <swarnikarajsingh@gmail.com>
2022-01-18 12:20:05 +0530

    jhkh

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,3 +1,4 @@
+import { Box, Button, Container, Divider, Grid, Heading, Input, Text, VStack } from '@chakra-ui/react';
 
 import { Link, useParams } from "react-router-dom"
 import { useNavigate } from "react-router-dom"
@@ -8,7 +9,9 @@ import { saveData } from "../../utils/localstore";
 import { useDispatch } from "react-redux"
 import { loginfailure, loginsuccess } from "../../featuresRedux/auth/action"
 
-export const Login=()=>{
+export const Login = () => {
+
+
     const navigate=useNavigate();
     const [form,setForm]=useState({});
     const dispatch=useDispatch()
@@ -21,56 +24,59 @@ export const Login=()=>{
            [name]:value
        })
    }
-   
-
-    
-    
 
 
-return <>
-    <h1>Login Page</h1>
-    <div>
-  
-    
-         <div style={{border:"2px solid"}}>
-            
-           <p> <input type="text" placeholder="Email" name="email"  onChange={handleChange} /></p> 
-
-           <p> <input type="text" placeholder="Password" name="password"  onChange={handleChange}/></p> 
-           
-           <button
-           
-           onClick={()=>{
-
-            try{
-        fetch("https://reqres.in/api/login",{
-            method:"POST",
-            body:JSON.stringify(form), 
-            headers:{"Content-Type":"application/json"}
-            ,
-        }).then(res=>res.json()).then(res=>{ dispatch(loginsuccess(res.token));console.log(res); navigate(-1)})
-     
-    }
-     catch(e){console.log(e);dispatch(loginfailure(e))}
-
-     
-           
-           }
-
-           }
-           
-           >Login</button>
-         
-            
-         </div>
-    
 
 
-    </div>
+    return (
+        <>
+            <Box bg={'#f0f2f5'} h={'700px'}>
+                <Grid templateColumns='repeat(2, 1fr)' maxW={'1100px'} m={'auto'} h={'600px'} >
 
+                    <Box mt={'160px'} py={5} ps={8} pe={2}>
+                        <Heading color={'#1877f2'} fontSize={60} mb={4}>facebook</Heading>
+                        <Text lineHeight={1.2} fontWeight={500} fontSize={26}>Facebook helps you connect and share with the people in your life.</Text>
+                    </Box>
 
-</>
+                    <Box >
+                        <Container h={'350px'} maxW={'400px'} mt={'120px'} bg={'white'} boxShadow={'lg'} rounded={10} p={4}>
+                            <VStack gap={2}>
+                                <Input type='email' placeholder='Email address or phone number' h={'50px'}  onChange={handleChange} />
+                                <Input type='password' placeholder='Password' h={'50px'}  onChange={handleChange}/>
+                                <Button w={'100%'} type='submit' bg={'#1877f2'} color={'white'} fontWeight={500} size='lg' _hover={{ bg: '#2572d6' }} fontSize={20}
+                                 onClick={()=>{
+
+try{
+    console.log(form, "I am from")
+fetch("https://reqres.in/api/login",{
+method:"POST",
+body:JSON.stringify(form), 
+headers:{"Content-Type":"application/json"}
+,
+}).then(res=>res.json()).then(res=>{ dispatch(loginsuccess(res.token));console.log(res); navigate(-1)})
+
+}
+catch(e){console.log(e);dispatch(loginfailure(e))}
 
 
 
 }
+
+}
+
+                                
+                                
+                                >Log In</Button>
+                                <Text>Forgotten password?</Text>
+                                <Divider />
+                                <Button size='lg' bg={'#42b72a'} fontWeight={500} color={'white'} _hover={{ bg: '#39a125' }}>Create New Account</Button>
+                            </VStack>
+                        </Container>
+                    </Box>
+
+                </Grid>
+
+            </Box>
+        </>
+    );
+};
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,10 +1,24 @@
-import { Login } from "../components/Login";
+import { Login } from "../components/auth/login";
+
+import {Routes, Route} from "react-router-dom";
+import {Homepage} from "../components/homepage/home";
+import {PrivateRoute} from "../components/auth/PrivateRouter"
 
 export const Router = () => {
 
     return  (
         <>
-        <Login />
+        <Routes>
+        <Route path={"/login"}  element={ <Login />}/>
+
+{/* private routing to access home page.. only authorised use are allowed    */}
+       <Route path={"/"} element={
+       <PrivateRoute>
+       <Homepage/>
+       </PrivateRoute>
+       }/>
+
+        </Routes>
         </>
     );
 };
