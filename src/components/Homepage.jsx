352c342d149b6946cf01257d3c6d7fae4d80cd08
bhargav katira <bhargavkatira@gmail.com>
2022-01-18 12:39:58 +0530

    homepage

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,7 @@
+export const Homepage = () => {
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+    )
+}
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,10 +1,16 @@
+import {Routes, Route} from "react-router-dom"
 import { Login } from "../components/Login";
+import {Homepage} from "../components/Homepage";
+
 
 export const Router = () => {
 
     return  (
         <>
-        <Login />
+        <Routes>
+       <Route path="/login" element={<Login />}></Route>
+       <Route path="/home" element={<Homepage />}></Route>
+        </Routes>
         </>
     );
 };
