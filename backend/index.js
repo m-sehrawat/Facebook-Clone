89ba07f3d77add9b0040f0a139d981ee00fa362d
swarnikaraj <swarnikarajsingh@gmail.com>
2022-01-18 15:58:52 +0530

    backendauth

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,11 @@
 const express=require('express');
-
+const {register,login}=require("./controllers/auth.controller")
 const app=express()
 
 app.use(express.json())
 
+app.post("register",register)
 
 
-
+app.post("/login",login)
 module.exports=app;
