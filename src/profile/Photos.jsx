baeaf96e1b33eb5f230bf5302717474cbd23e46c
Mohit Sehrawat <mohitsehrawat000@gmail.com>
2022-01-19 18:44:13 +0530

    nested routers added

---
+++ src/profile/Photos.jsx
@@ -0,0 +1,13 @@
+import { Box, Heading } from "@chakra-ui/react";
+
+export const Photos = () => {
+    return (
+        <>
+            <Box bg={'#f0f2f5'} minH={'200px'}>
+
+                <Heading>Photos</Heading>
+                
+            </Box>
+        </>
+    );
+};
