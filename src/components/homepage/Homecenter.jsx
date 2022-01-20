ce0b2b746806ea1c320d89cd7782c8d01b036ceb
bhargav katira <bhargavkatira@gmail.com>
2022-01-20 13:11:05 +0530

    updating story reel

---
+++ src/components/homepage/Homecenter.jsx
@@ -0,0 +1,12 @@
+import React from "react"; 
+import {StoryReel}  from "./StoryReel"
+import "./homecenter.css";
+
+export const Homecenter = () => {
+    return (
+        <div className="Homecenter">
+        <StoryReel  />
+        
+        </div>
+    )
+}
