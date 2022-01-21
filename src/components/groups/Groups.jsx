604110204a7376727f7921ecfde3db185703b03e
Mohit Sehrawat <mohitsehrawat000@gmail.com>
2022-01-21 21:21:40 +0530

    updated

--- src/components/groups/Groups.jsx
+++ src/components/groups/Groups.jsx
@@ -18,22 +18,18 @@ const GroupBox = ({ title, src }) => {
 
 export const Groups = () => {
 
-
-
     return (
-        <>
-            <Box minH={'660px'} bg={'#f0f2f5'} pb={'80px'} border={'1px solid #f0f2f5'}>
-                <Box w={'1100px'} m={'auto'} >
-                    <Heading fontSize={20} my={5}>Popular near you</Heading>
-                </Box>
-                <SimpleGrid columns={3} spacing={5} w={'1100px'} m={'auto'} minH={200}>
-
-                    {[1, 2, 3, 4, 5, 6].map((e) => (
-                        <GroupBox title={'Title'} src={"https://via.placeholder.com/350x200"} />
-                    ))}
-
-                </SimpleGrid>
+        <Box minH={'660px'} bg={'#f0f2f5'} pb={'80px'} border={'1px solid #f0f2f5'}>
+            <Box w={'1100px'} m={'auto'} >
+                <Heading fontSize={20} my={5}>Popular near you</Heading>
             </Box>
-        </>
+            <SimpleGrid columns={3} spacing={5} w={'1100px'} m={'auto'} minH={200}>
+
+                {[1, 2, 3, 4, 5, 6].map((e) => (
+                    <GroupBox title={'Title'} src={"https://via.placeholder.com/350x200"} />
+                ))}
+
+            </SimpleGrid>
+        </Box>
     );
 };
