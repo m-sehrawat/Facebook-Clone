9c7a4d7eb3573b4bc738ca6b98573b359e7b8852
Mohit Sehrawat <mohitsehrawat000@gmail.com>
2022-01-22 00:13:05 +0530

    updated

--- src/components/bkwas.jsx
+++ src/components/bkwas.jsx
@@ -1,45 +1,57 @@
 
-import { useState , useEffect} from "react";
+import { useState, useEffect } from "react";
+
 import { loadData } from '../utils/localstore';
 
-export const Bkwas=()=>{
-const [frdids,setFrdids]=useState([])
-const [item,setItem]=useState({})
-const {_id}=loadData('user')
+export const Bkwas = () => {
+  const [frdids, setFrdids] = useState([])
+  console.log('frdids:', frdids)
+ 
+  const [arr, setArr] = useState([])
+  console.log('arrOutside:', arr)
 
+  const { _id } = loadData('user')
 
-
-   function getdata(id){
-
-    fetch(`http://localhost:1234/user/${id}`).then(d=>d.json()).then(res=> setFrdids(res.friend_ids))
-       .catch(err=>{console.log(err)})
-   }
   
 
-    useEffect(()=>{
-      getdata(_id)
-    },[])
-   
-    
-     
 
-    
-    useEffect(()=>{
-      frdids.forEach((userid)=>{
+  function getdata(id) {
 
-        fetch(`http://localhost:1234/user/${userid}`).then(d=>d.json()).then(res=>console.log(res))
-            .catch(err=>{console.log(err)})
+    fetch(`http://localhost:1234/user/${id}`).then(d => d.json()).then(res => setFrdids(res.friend_ids))
+      .catch(err => { console.log(err) })
+  }
+
+
+  useEffect(() => {
+    getdata(_id)
+  }, [])
+
+
+  useEffect(() => {
+
+    frdids.forEach((id) => {
+      fetch(`http://localhost:1234/user/${id}`)
+        .then((res) => res.json())
+        .then((res) => {
+          arr.push(res)
           
-      })  
+          setArr([...arr]);
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+    })
 
-    },[frdids])
-       
+  }, [ frdids])
+
+
   
 
 
 
 
-return <div>Hello mai v bak hu frd list k liye</div>
+
+  return <div>Hello mai v bak hu frd list k liye</div>
 
 
 
@@ -48,9 +60,9 @@ return <div>Hello mai v bak hu frd list k liye</div>
 }
 
 
- 
 
-    
+
+
 
 
 
