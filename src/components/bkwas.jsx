f9541e3cb286ebb19db683372331e77626de7055
swarnikaraj <swarnikarajsingh@gmail.com>
2022-01-21 22:05:54 +0530

    jbkj

--- src/components/bkwas.jsx
+++ src/components/bkwas.jsx
@@ -3,16 +3,17 @@ import { useState , useEffect} from "react";
 import { loadData } from '../utils/localstore';
 
 export const Bkwas=()=>{
-
+const [frdids,setFrdids]=useState([])
 const {_id}=loadData('user')
 
    function getdata(id){
 
-    fetch(`http://localhost:1234/user/${id}`).then(d=>d.json()).then(res=>console.log(res.friend_ids))
-
+    fetch(`http://localhost:1234/user/${id}`).then(d=>d.json()).then(res=> setFrdids(res.friend_ids); console.log(res.friend_ids))
+       .catch(err=>{console.log(err)})
    }
   
-  
+
+    
    
   getdata(_id)
     
