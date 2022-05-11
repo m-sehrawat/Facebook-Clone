e515d14d0b17b0fab465ce004f563f2c4490d047
Mohit Sehrawat <mohitsehrawat000@gmail.com>
2022-05-11 15:48:15 +0530

    cors error fixed

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser')
+const cors = require('cors');
+const app = express();
 
-const app = express()
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
 
 app.use(
   bodyParser.urlencoded({
